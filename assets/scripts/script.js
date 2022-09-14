65528b00bca1c65cc9796035b6571ffeb92ad921
PBP66 <53805701+pbp66@users.noreply.github.com>
2022-09-13 21:07:12 -0400

    Added password object, password criteria input and validation

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,6 +3,39 @@
 
 function generatePassword() {
 
+	var password = {
+		length: 0,
+		lowercase: "",
+		uppercase: "",
+		numeric: "",
+		specialCharacters: "",
+		validateLength: function() {
+			return this.length >= 8 && this.length <= 128;
+		},
+		validate: function () {
+			return this.lowercase || this.uppercase || this.numeric || this.specialCharacters;
+		}
+	}
+
+	// Taking Input
+	while (true) {
+		password.length = parseInt(prompt("Enter a password length between 8 and 128 characters inclusive:"));
+		if (password.validateLength()) {
+			password.lowercase = prompt("Include lowercase characters?\nEnter yes or no.").toLowerCase() === 'yes';
+			password.uppercase = prompt("Include uppercase characters?\nEnter yes or no.").toLowerCase() === 'yes';
+			password.numeric = prompt("Include numbers?\nEnter yes or no.").toLowerCase() === 'yes';
+			password.specialCharacters = prompt("Include special characters?\nEnter yes or no.").toLowerCase() === 'yes';
+			
+			if (password.validate()) {
+				break;
+			} else {
+				alert("You must specify at least one of the following character types");
+			}
+
+		} else {
+			alert("The length entered was incorrect. Length must be between 8 and 128 inclusive.");
+		}
+	}
 }
 
 // Get references to the #generate element
@@ -12,7 +45,7 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
 	var password = generatePassword();
   	var passwordText = document.querySelector("#password");
-	
+
   	passwordText.value = password;
 
 }
